aa149db9f534c686168bc4f35c699db849449437
luchetu <humluchetu@gmail.com>
2020-06-09 10:58:56 +0300

    finnished bookstore graphql project

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const graphqlHTTP=require('express-graphql');
+const mongoose=require('mongoose');
+const schema = require('./schema/schema');
+const cors = require('cors');
 
 const app=express();
-app.app('/graphql'.graphqlHTTP({
-schema,
-graphiql: true
+
+// allow cross-origin requests
+app.use(cors());
+
+mongoose.connect('mongodb+srv://hluchetu:hum1234@cluster0-6frs1.mongodb.net/Cluster0?retryWrites=true&w=majority');
+mongoose.connection.once('open',()=>{
+    console.log("connected to database");
+});
+
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
 }));
 
 app.listen(4000,()=>{
